0085cabaf2ddc14d158faa7a4bfe72b44a9c92db
Hemajita Sahoo <hemajita14@gmail.com>
2024-04-20 14:03:55 +0530

    added "optaional" class

--- Optional/OpationalClass.java
+++ Optional/OpationalClass.java
@@ -1,5 +1,19 @@
 package Optional;
 
-public class OpationalClass {
 
+import java.util.Optional;
+
+public class OpationalClass {
+public static void main(String[] args){
+String[] str=new String[10];
+//str[5]="Hello";
+    Optional<String> checknull=Optional.ofNullable(str[5]);
+if(checknull.isPresent()){
+    String s=str[5].toLowerCase();
+    System.out.println(s);
+}
+   else{
+       System.out.println("String value is not present");
+}
+}
 }
