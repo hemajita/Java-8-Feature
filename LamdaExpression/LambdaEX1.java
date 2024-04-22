f15376467f0a14ca8f5a6a6be09a95918687ed1b
Hemajita Sahoo <hemajita14@gmail.com>
2024-04-22 14:01:42 +0530

    added even and odd number using lambda expression

---
+++ LamdaExpression/LambdaEX1.java
@@ -0,0 +1,26 @@
+package LamdaExpression;
+
+import java.util.ArrayList;
+
+public class LambdaEX1 {
+    public static void main(String[] args){
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        arr.add(14);
+        arr.add(10);
+        arr.add(18);
+        arr.add(15);
+        arr.add(11);
+        arr.add(13);
+
+        arr.forEach(n->System.out.println(n));
+//for even number
+        arr.forEach(n->{if (n%2==0)
+        System.out.println(n);
+        });
+//for odd number
+        arr.forEach(n->{if ((n%2)!=0)
+            System.out.println(n);
+        });
+
+    }
+}
